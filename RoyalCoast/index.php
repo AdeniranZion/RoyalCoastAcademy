8933326ae789b490497c19688a42de8f8411576f
AdeniranZion <58471753+AdeniranZion@users.noreply.github.com>
2022-09-15 00:53:47 +0100

    Add files via upload

---
+++ RoyalCoast/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Royal Coast Academy</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+        <!-- Navbar Start -->
+        <div class="hero">
+            <nav>
+                <ul>
+                    <li><a href="#index" a:hover{color: rgb(0, 0, 0);transition: .3s;} style="color: #F9A8F6; ">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                    <li><a href="#">ADMISSIONS</a></li>
+                    <li><a href="#">EVENTS</a></li> 
+                    <li><a href=""><img src="images/ic_view_headline_24px.png"></a></li>   
+                </ul>
+            </nav>
+        </div>
+        <!-- Navbar End -->
+
+        <div class="subhero">
+                 <a href="" style="justify-self: left; align-self:center;"><img src="images/Goback1.png"></a>
+                 <a href="" style="justify-self: left; align-self:center; margin:1390px;"><img src="images/Goforward.png"></a>
+                 <a href="" style="justify-self: left; align-self: center; margin:-2100px; margin-top: -1560px"><img src="images/BLocator.png"></a>
+                 <a href="#"  style="float: center; margin-left:1450px; margin-top:510px;"><img src="images/Button.png"></a>
+                 <a href="#"  style="float: center; margin-left:-354px; margin-top:-90px;"><img src="images/RCALogoBar.png"></a>
+        </div>
+        
+        <!-- Welcome to Royal Coast-->
+        <div class="class2">
+                <!-- <a href="" style="margin: 25px;"><img src="images/list-Curriculum Thumb.png"></a> -->
+                <a href="#" style="float: center; margin-left:-960px; margin-top:70px;"><img src="images/list-Curriculum Thumb.png"></a>
+                
+                <h1 style="font-size: 50px; font-weight:bold; color:black; float: center; margin-left:-160px; margin-top:-260px;">Welcome to <br>Royal Coast Academy<br></h1>
+                <h4 style="font-size: 20px; line-height: 1.8; margin-left: 35px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim,<br>non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum.<br>Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat.</h4>
+                <a href="#" style="float: center; margin-left:400px; margin-top:10px;"><img src="images/Button1.png"></a>
+        </div>
+
+        <!-- About Us -->
+        <div class="plain">
+                 <a href="#" style="float: center; margin-left: 580px; margin-top:90px;"><img src="images/ab2.png"></a>
+                <a href="#" style="float: center; margin-left:-700px; margin-top: -300px;"><img src="images/AboutUs.png"></a>
+                
+                <h4 style="font-size: 20px; line-height: 1.8; margin-left:-340px; margin-top:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>Cras rutrum iaculis enim, non convallis felis mattis at Donec fringilla.<br>Etiam mattis eros eu ullamcorper volutpat.</h4>
+                <a href="#" style="float: center; margin-left:-800px; margin-top:20px; "><img src="images/Button1.png"></a>
+        </div>
+        <!-- About Us End -->
+
+        <div class="class2" style="background-color: #F5C3F3; height: 92vh;">
+                <a href="#" style="float: center; margin-left: 70px; margin-top:40px;"><img src="images/Photo Gallery.png"></a> 
+                <div class="class2" style="background-image: url(../images/3BDA3943B0350E099134905644BAFF5E.png); margin-top:40px; height: 80vh; background-size: cover; background-position: center;">
+                        <a href="#" style="height: 10vh; justify-self: left; align-self:center; object-fit:cover;"><img src="images/3BDA3943B0350E099134905644BAFF5E.jpg"></a>
+                </div>  
+        </div>
+
+        <div class="plain" style="height: 70vh;"></div>
+
+        <div class="class2" style="background-image: url(../images/EEDAA7809838AFC32DCDE27060EAAE41.png); background-size: cover; background-position: center; height: 80.6vh;">
+                <a href="#" style="float: center; background-size: 100px; "><img src="images/EEDAA7809838AFC32DCDE27060EAAE41.jpg"></a>  
+        </div>
+
+        <!-- Contact Us Div -->
+        <div class="plain" style="height: 90vh;">
+                <!-- <a href="#" style="float: center; margin-left: 60px; margin-top:60px;"><img src="images/ContUs.png"></a>  -->
+                <h1 style="margin-top: 100px; margin-left: -80px; flex-direction: row; font-size: 50px;">Contact<h1 style="color: #F670F1; margin-left: 180px; margin-top: -68px; font-size: 50px;">Us</h1></h1> 
+                <h3 style="margin-top: 10px; margin-bottom: 40px">You can send a message. We'll be delighted to hear from you</h3>
+
+                <!-- Contact Form -->
+                <form style="float: left; margin-left: 350px; box-sizing:border-box; align-self:flex-start; margin-top: 10px;">
+                    <input type="text" id="fname" name="fname" placeholder="Full Name">
+                    <input type="text" id="email" name="emailadd" placeholder="Email Address">
+                </form>
+                <form style="float: right; margin-top: -135px; margin-right: 220px; box-sizing:border-box; align-self:flex-end;">
+                    <input type="text" id="phonenum" name="phonenum" placeholder="Phone Number">
+                    <input type="text" id="subject" name="subject" placeholder="Subject">
+                </form>
+                <form style="float: right; width:71.6%; margin-left: 256px; padding:8px; margin-top: 5px;">
+                    <!-- <textarea border-bottom: 3.5px solid #F670F1; name = "Message" rows="5" cols="20"></textarea> -->
+                    <input type="text" id="fname" name="fname" placeholder="Message">
+                </form>
+                <a href="#"  style="float: center; margin-left:770px; margin-top:20px;"><img src="images/Button2.png"></a>
+                <!-- Contact Form Ends -->
+
+        </div>
+        <!-- Contact Us Ends -->
+
+
+        <!-- Footer Starts -->
+        <div class="footer">
+                <!-- Reach Us -->
+              <h2 style="color: #b00b69; margin-top: 70px; margin-left:-1176px; font-weight:bold; font-size: 35px;">Reach Us</h2>  
+              <h5 style="color: #b00b69; margin-top: 20px; margin-left:-1060px; line-height: 1.8;">10 Oluwole Daramola,<br>Off Mabinuori, Opic Bus stop,<br>Lagos - Ibadan Expressway,<br>Ogun State.</h5>  
+              <a href="#"  style="float: center; margin-left:-1400px; margin-top: -110px;"><img src="images/ic_place_24px.png"></a>
+              <a href="#"  style="float: center; margin-left:-1400px; margin-top: 80px;"><img src="images/ic_phone_in_talk_24px.png"></a>
+              <h5 style="color: #b00b69; margin-top: -37px; margin-left:-1148px; line-height: 1.8;">+234 802 766 4776</h5>  
+                <!-- Reach Us Ends -->
+
+                <!-- Quick Links -->
+                <h2 style="color: #b00b69; margin-top: -275px; margin-left: -20px; font-weight:bold; font-size: 35px;">Quick Links</h2> 
+                
+                    <ol style="list-style-type: none; line-height:1.9; color: #b00b69; flex-direction: column; margin-left:-84px;">
+                        <li><a href="" a:hover{color: rgb(0, 0, 0);transition: .3s;} style="color:#b00b69;">Admissions</a></li>
+                        <li><a href="#" style="color:#b00b69;">News and Events</a></li>
+                        <li><a href="#" style="color:#b00b69;">Photo Gallery</a></li>
+                        <li><a href="#" style="color:#b00b69;">Parents Forum</a></li> 
+                        <li><a href="#" style="color:#b00b69;">Job Oppurtunities</a></li> 
+                        <li><a href="#" style="color:#b00b69;">FAQs</a></li> 
+                        <li><a href="#" style="color:#b00b69;">Help and Support</a></li> 
+                        <li><a href="#" style="color:#b00b69;">Job Oppurtunities</a></li> 
+                    </ol>
+                <!-- Quick Links Ends -->
+                <h2 style="color: #b00b69; margin-top: -310px; margin-left: 950px; font-weight:bold; font-size: 35px;">Newsletter</h2> 
+
+              <!-- Footer Line -->
+              <div class = "futz"></div>
+
+        </div>
+       <!-- Footer Ends -->
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/popper.min.js"></script>
+</body>
+</html>
